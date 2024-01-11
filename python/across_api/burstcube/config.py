94c325fc79d49a0ad2a06006b133965e7378c611
Jamie Kennea <jak574@gmail.com>
2024-01-11 12:32:03 -0600

    Add copyright

--- python/across_api/burstcube/config.py
+++ python/across_api/burstcube/config.py
@@ -1,3 +1,7 @@
+# Copyright © 2023 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+
 import astropy.units as u  # type: ignore
 import numpy as np
 from astropy.time import Time  # type: ignore
