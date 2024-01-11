7340415bf14bfd6d8973b575d8247abcb3d50cde
Jamie Kennea <jak574@gmail.com>
2024-01-10 21:15:12 -0600

    Add spacetrack secrets

--- python/across_api/api_secrets.py
+++ python/across_api/api_secrets.py
@@ -1,7 +1,6 @@
 # Copyright © 2023 United States Government as represented by the
 # Administrator of the National Aeronautics and Space Administration.
 # All Rights Reserved.
-
 import os
 
 # Database Login Information
@@ -10,3 +9,7 @@ ACROSS_DB_PASSWD = os.environ["ACROSS_DB_PASSWD"]
 ACROSS_DB_HOST = os.environ["ACROSS_DB_HOST"]
 ACROSS_DB_PORT = os.environ["ACROSS_DB_PORT"]
 ACROSS_DB_NAME = os.environ["ACROSS_DB_NAME"]
+
+# space-track.org login information
+SPACE_TRACK_USER = os.environ["SPACE_TRACK_USER"]
+SPACE_TRACK_PASS = os.environ["SPACE_TRACK_PASS"]
