be380ffb0e8fa63bc7180b2c0daedab6ef011910
Jamie Kennea <jak574@gmail.com>
2024-01-31 14:24:25 -0500

    Update SAA constraint classes

--- python/across_api/burstcube/saa.py
+++ python/across_api/burstcube/saa.py
@@ -4,7 +4,7 @@
 
 from typing import Type
 
-from ..base.constraints import SAAPolygonConstraint  # type: ignore
+from .constraints import burstcube_saa_constraint
 from ..base.saa import SAABase
 from .ephem import BurstCubeEphem
 
@@ -15,29 +15,7 @@ class BurstCubeSAA(SAABase):
     """
 
     # BurstCube Spacecraft SAA polygon as supplied by BurstCube team.
-    insaacons = SAAPolygonConstraint(
-        polygon=[
-            (33.900000, -30.0),
-            (12.398, -19.876),
-            (-9.103, -9.733),
-            (-30.605, 0.4),
-            (-38.4, 2.0),
-            (-45.0, 2.0),
-            (-65.0, -1.0),
-            (-84.0, -6.155),
-            (-89.2, -8.880),
-            (-94.3, -14.220),
-            (-94.3, -18.404),
-            (-84.48631, -31.84889),
-            (-86.100000, -30.0),
-            (-72.34921, -43.98599),
-            (-54.5587, -52.5815),
-            (-28.1917, -53.6258),
-            (-0.2095279, -46.88834),
-            (28.8026, -34.0359),
-            (33.900000, -30.0),
-        ]
-    )
+    insaacons = burstcube_saa_constraint
 
     def __init__(self, ephemclass: Type[BurstCubeEphem] = BurstCubeEphem, **kwargs):
         """
