134b48322ae66ed32990bde9f9da969577f0a79e
Jamie Kennea <jak574@gmail.com>
2024-02-11 08:16:33 -0500

    Format fix breaking CI

--- python/tests/constraints/test_swift_saa.py
+++ python/tests/constraints/test_swift_saa.py
@@ -10,4 +10,3 @@ def test_swift_saa(swift_ephem, swiftapi_saa_entries, swift_saa_entries):
     # Convert this to a list of start/stop windows
     assert len(swiftapi_saa_entries) == len(swift_saa_entries)
     assert (np.abs(swift_saa_entries - swiftapi_saa_entries) <= 60).all()
-    
